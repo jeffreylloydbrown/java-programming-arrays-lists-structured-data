4e6002ecc5ddca8d7c8aa58af206be4ecf894bea
Brown <jeffrey.l.brown@thomsonreuters.com>
2018-02-25 18:31:40 -0600

    Finished the implementation from the class videos.

--- Week2/random-story/src/WordFrequencies.java
+++ Week2/random-story/src/WordFrequencies.java
@@ -4,9 +4,11 @@ import java.util.ArrayList;
 public class WordFrequencies {
 
     private ArrayList<String> myWords;
+    private ArrayList<Integer> myFreqs;
 
     public WordFrequencies () {
         myWords = new ArrayList<String>();
+        myFreqs = new ArrayList<Integer>();
     }
 
     public void findUnique () {
@@ -16,7 +18,15 @@ public class WordFrequencies {
             s = s.toLowerCase();
             int index = myWords.indexOf(s);
             if (index == -1) {
+                // word not found, remember it and set the count
+                // for the word to 1.
                 myWords.add(s);
+                myFreqs.add(1);
+            } else {
+                // we've seen this word before, and `index` is
+                // its location.  So bump that location in `myFreqs`.
+                int value = myFreqs.get(index);
+                myFreqs.set(index, value + 1);
             }
         }
     }
@@ -24,5 +34,8 @@ public class WordFrequencies {
     public void tester() {
         findUnique();
         System.out.println("# unique words: "+myWords.size());
+        for (int k = 0; k < myWords.size(); k++) {
+            System.out.println(myFreqs.get(k)+"\t"+myWords.get(k));
+        }
     }
 }
