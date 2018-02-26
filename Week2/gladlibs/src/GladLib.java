375f85988efef1128b7d306825f65080fb1e1d21
Brown <jeffrey.l.brown@thomsonreuters.com>
2018-02-26 12:11:55 -0600

    Added verb and fruit support

--- Week2/gladlibs/src/GladLib.java
+++ Week2/gladlibs/src/GladLib.java
@@ -9,6 +9,8 @@ public class GladLib {
 	private ArrayList<String> nameList;
 	private ArrayList<String> animalList;
 	private ArrayList<String> timeList;
+	private ArrayList<String> verbList;
+	private ArrayList<String> fruitList;
 	
 	private Random myRandom;
 	
@@ -32,7 +34,9 @@ public class GladLib {
 		countryList = readIt(source+"/country.txt");
 		nameList = readIt(source+"/name.txt");		
 		animalList = readIt(source+"/animal.txt");
-		timeList = readIt(source+"/timeframe.txt");		
+		timeList = readIt(source+"/timeframe.txt");
+		verbList = readIt(source+"/verb.txt");
+		fruitList = readIt(source+"/fruit.txt");
 	}
 	
 	private String randomFrom(ArrayList<String> source){
@@ -61,6 +65,12 @@ public class GladLib {
 		}
 		if (label.equals("timeframe")){
 			return randomFrom(timeList);
+		}
+		if (label.equals("verb")){
+			return randomFrom(verbList);
+		}
+		if (label.equals("fruit")){
+			return randomFrom(fruitList);
 		}
 		if (label.equals("number")){
 			return ""+myRandom.nextInt(50)+5;
@@ -128,7 +138,7 @@ public class GladLib {
 	
 	public void makeStory(){
 	    System.out.println("\n");
-		String story = fromTemplate("src/data/madtemplate.txt");
+		String story = fromTemplate("src/data/madtemplate2.txt");
 		printOut(story, 60);
 	}
 	
